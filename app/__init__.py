1be04f80a320c4c372aae99fb3bce05a907665e0
Faith Nchang <143855102+Faith-Nchang@users.noreply.github.com>
2025-08-15 14:14:16 -0400

    created basic landing page

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from app.routes.init import register_routes
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static', template_folder='templates')
     app.config.from_object('config.Config')
+
+    from app.routes.init import register_routes
+
 
     # Initialize routes
     register_routes(app)
